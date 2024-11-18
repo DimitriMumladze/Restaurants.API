d187cf8480eeebe16bc2e990f2b94b7e54c036c5
Dimitri <kenwo.dm@gmail.com>
2024-11-18 15:44:58 +0400

    I just added request time logging middleware

--- Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
+++ Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b433124870c598a059f6e3de81269f9cb955d3c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b891de4f427b57412860e599b5e76f4f19652a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
