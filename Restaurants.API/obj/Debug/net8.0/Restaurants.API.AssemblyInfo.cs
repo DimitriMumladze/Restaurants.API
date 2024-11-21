b37d96e171fd1d3a9a49275c282797062fc0103d
Dimitri <kenwo.dm@gmail.com>
2024-11-21 08:50:05 +0400

    Added custom requirement of age require logic [example 20 years old]

--- Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
+++ Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+119e23c71ee5d19a93986ec8c87286d6bf209361")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+053d7e1cb5e4e4d2d883ff266aea1e6d6a11839a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Restaurants.Application/Users/UserContext.cs
+++ Restaurants.Application/Users/UserContext.cs
@@ -16,12 +16,17 @@ public class UserContext(IHttpContextAccessor httpContextAccessor) : IUserContex
         if (user.Identity == null || user.Identity.IsAuthenticated)
             return null;
 
-        //now true variation
+        //now true variation //getting values
         var userId = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)!.Value;
         var email = user.FindFirst(c => c.Type == ClaimTypes.Email)!.Value;
         var roles = user.Claims.Where(c => c.Type == ClaimTypes.Role)!.Select(c => c.Value);
+        var nationality = user.FindFirst(c => c.Type == "Nationality")?.Value;
+        var dateOfBirthString = user.FindFirst(c => c.Type == "DateOfBirth")?.Value;
+        var dateOfBirth = dateOfBirthString == null
+            ? (DateOnly?)null
+            : DateOnly.ParseExact(dateOfBirthString, "yyyy-MM-dd");
 
-        return new CurrentUser(userId, email, roles);
+        return new CurrentUser(userId, email, roles, nationality, dateOfBirth);
     }
 }
 
